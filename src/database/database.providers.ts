8b4b5ff68b3dae0d3fd2a7972e126196fffb5da0
Sang Dang <sang.a5vy2@gmail.com>
2019-03-14 09:03:18 +0700

    change database config

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -2,6 +2,26 @@ import { ProductEnt } from './../product/product.entity';
 import { User } from './../user/user.entity';
 import { Sequelize } from 'sequelize-typescript';
 
+const localDb = {
+  operatorsAliases: false,
+  dialect: 'mysql',
+  host: 'localhost',
+  port: 3306,
+  username: 'root',
+  password: 'root',
+  database: 'api',
+};
+
+const herokuDb = {
+  operatorsAliases: false,
+  dialect: 'mysql',
+  host: 'us-cdbr-iron-east-03.cleardb.net',
+  port: 3306,
+  username: 'b184f84a347e15',
+  password: '004d043a',
+  database: 'heroku_3a3ebe478dc35e1',
+};
+
 export const databaseProviders = [
   {
     provide: 'SequelizeToken',
@@ -9,11 +29,11 @@ export const databaseProviders = [
       const sequelize = new Sequelize({
         operatorsAliases: false,
         dialect: 'mysql',
-        host: 'localhost',
+        host: 'us-cdbr-iron-east-03.cleardb.net',
         port: 3306,
-        username: 'root',
-        password: 'root',
-        database: 'api',
+        username: 'b184f84a347e15',
+        password: '004d043a',
+        database: 'heroku_3a3ebe478dc35e1',
       });
       sequelize.addModels([
         ProductEnt,
