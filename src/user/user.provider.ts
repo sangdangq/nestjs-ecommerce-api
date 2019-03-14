8a47d1957717127707d4121c5efd7b0013eb0257
HARVEYNASH\sangdangq <shiorsher@outlook.com.vn>
2019-03-14 13:58:13 +0700

    define node version package.json

--- src/user/user.provider.ts
+++ src/user/user.provider.ts
@@ -1,13 +1,9 @@
-import { User, ResetPassword, AccessToken } from './user.entity';
+import { User, AccessToken } from './user.entity';
 
 export const userProviders = [
   {
     provide: 'UserRepository',
     useValue: User,
-  },
-  {
-    provide: 'ResetPasswordRepository',
-    useValue: ResetPassword,
   },
   {
     provide: 'AccessTokenRepo',
