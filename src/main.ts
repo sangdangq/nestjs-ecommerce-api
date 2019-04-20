3b0a48628b5fc4a7390fd928bf3e046cab758aa7
Sang Dang <sang.a5vy2@gmail.com>
2019-04-21 00:18:57 +0700

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ import 'reflect-metadata';
 declare const module: any;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new AllExceptionsFilter());
 
