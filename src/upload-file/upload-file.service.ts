034d15e075f6425d8008b27fdc565c42a2ad1110
Sang Dang <sang.a5vy2@gmail.com>
2019-04-26 00:32:17 +0700

    remove cloudinary library

--- src/upload-file/upload-file.service.ts
+++ src/upload-file/upload-file.service.ts
@@ -1,6 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { Cloudinary } from '@cloudinary/angular-5.x';
-import * as cloudinary from 'cloudinary';
 
 @Injectable()
 export class UploadFileService {
