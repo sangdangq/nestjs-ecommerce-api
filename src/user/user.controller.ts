a1ea7a5e39194a6ce706497222fbc32c2c625b95
Sang Dang <sang.a5vy2@gmail.com>
2020-02-09 20:37:04 +0700

    get user by email

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,40 +1,51 @@
-import { HttpStatus, Put, UseGuards, Get } from '@nestjs/common';
+import { HttpStatus, Put, UseGuards, Get, Param } from '@nestjs/common';
 import { Controller, Post, Body, Res } from '@nestjs/common';
-import { ApiBadRequestResponse, ApiOkResponse, ApiBearerAuth } from '@nestjs/swagger';
+import { ApiOkResponse, ApiBearerAuth } from '@nestjs/swagger';
 import { UserService } from './user.service';
-import { LoginVm, TokenVm, UserRegisterVm, RefreshTokenVm, UserUpdate } from './user.model';
+import {
+  LoginVm,
+  TokenVm,
+  UserRegisterVm,
+  RefreshTokenVm,
+  UserUpdate
+} from './user.model';
 import { AuthGuard } from '@nestjs/passport';
 
 @Controller('user')
 export class UserController {
-  constructor(
-    private readonly _userService: UserService) {}
+  constructor(private readonly _userService: UserService) {}
   public errMsg = 'Authorization information is missing or invalid.';
 
-  @ApiOkResponse({type: TokenVm})
+  @ApiOkResponse({ type: TokenVm })
   @Post('login')
   async login(@Body() body: LoginVm, @Res() res) {
-    return this._userService.login(body).subscribe(data => {
-      res.status(HttpStatus.OK).send(data);
-    }, err => {
-      res.status(HttpStatus.BAD_REQUEST).end(err.response.data);
-    });
+    return this._userService.login(body).subscribe(
+      data => {
+        res.status(HttpStatus.OK).send(data);
+      },
+      err => {
+        res.status(HttpStatus.BAD_REQUEST).end(err.response.data);
+      },
+    );
   }
 
-  @ApiOkResponse({type: TokenVm})
+  @ApiOkResponse({ type: TokenVm })
   @Post('register')
   async register(@Body() registerInfo: UserRegisterVm, @Res() res) {
-    return this._userService.register(registerInfo).subscribe(data => {
-      res.status(HttpStatus.OK).send(data);
-    }, err => {
-      res.status(HttpStatus.BAD_REQUEST).end(err.response.data);
-    });
+    return this._userService.register(registerInfo).subscribe(
+      data => {
+        res.status(HttpStatus.OK).send(data);
+      },
+      err => {
+        res.status(HttpStatus.BAD_REQUEST).end(err.response.data);
+      },
+    );
   }
 
   @UseGuards(AuthGuard())
   @ApiBearerAuth()
   @Put('updateProfile')
-  async updateProfile( @Body() body: UserUpdate, @Res() res ) {
+  async updateProfile(@Body() body: UserUpdate, @Res() res) {
     const result = await this._userService.updateProfile(body);
     if (result.isSuccess) {
       res.status(HttpStatus.OK).send(result.message);
@@ -43,24 +54,42 @@ export class UserController {
     }
   }
 
-  @ApiOkResponse({type: TokenVm})
+  @ApiOkResponse({ type: TokenVm })
   @Post('refreshToken')
   async refreshToken(@Body() refreshInfo: RefreshTokenVm, @Res() res) {
-    return this._userService.refreshToken(refreshInfo).subscribe(data => {
-      res.status(HttpStatus.OK).send(data);
-    }, err => {
-      res.status(HttpStatus.BAD_REQUEST).send(err.response.data);
-    });
+    return this._userService.refreshToken(refreshInfo).subscribe(
+      data => {
+        res.status(HttpStatus.OK).send(data);
+      },
+      err => {
+        res.status(HttpStatus.BAD_REQUEST).send(err.response.data);
+      }
+    );
   }
 
   @UseGuards(AuthGuard())
   @ApiBearerAuth()
   @Get('listUser')
   listUser(@Res() res) {
-    this._userService.getlistUser().then(data => {
-      res.status(HttpStatus.OK).send(data);
-    }).catch(err => {
-      res.status(HttpStatus.BAD_REQUEST).send(err);
-    });
+    this._userService
+      .getlistUser()
+      .then(data => {
+        res.status(HttpStatus.OK).send(data);
+      })
+      .catch(err => {
+        res.status(HttpStatus.BAD_REQUEST).send(err);
+      });
+  }
+
+  @UseGuards(AuthGuard())
+  @ApiBearerAuth()
+  @Get(':email')
+  async userProfile(@Res() res, @Param('email') email: string) {
+    const profile = await this._userService.getProfileByEmail(email);
+    if (profile) {
+      res.status(HttpStatus.OK).send(profile);
+    } else {
+      res.status(HttpStatus.BAD_REQUEST).end('Failed to get user profile');
+    }
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,99 +1,110 @@
 import { Injectable, Inject } from '@nestjs/common';
+import { InjectMapper, AutoMapper } from 'nestjsx-automapper';
 import { HttpService } from '@nestjs/common/http';
 import { map } from 'rxjs/operators';
-import { LoginVm, UserRegisterVm, RefreshTokenVm, UserUpdate } from './user.model';
+import {
+  LoginVm,
+  UserRegisterVm,
+  RefreshTokenVm,
+  UserUpdate,
+} from './user.model';
 import { User } from './user.entity';
 import * as crypto from 'crypto-js';
 
 @Injectable()
 export class UserService {
-    constructor(
-        private readonly _http: HttpService,
-        @Inject('UserRepository') private readonly userRepo: typeof User,
-    ) {}
+  constructor(
+    private readonly _http: HttpService,
+    @Inject('UserRepository') private readonly userRepo: typeof User,
+    @InjectMapper() private readonly _mapper: AutoMapper,
+  ) {}
 
-    public identityUrl = 'https://idensd.herokuapp.com/user/';
+  public identityUrl = 'https://idensd.herokuapp.com/user/';
 
-    login(userCre: LoginVm) {
-        const endpoint = this.identityUrl + 'login';
-        return this._http.post(endpoint, userCre)
-            .pipe(
-                map(response => response.data),
-            );
+  login(userCre: LoginVm) {
+    const endpoint = this.identityUrl + 'login';
+    return this._http
+      .post(endpoint, userCre)
+      .pipe(map(response => response.data));
+  }
+
+  async getUserbyEmail(userEmail: string) {
+    return this.userRepo.findOne({
+      where: { email: userEmail }
+    });
+  }
+
+  register(registerInfo: UserRegisterVm) {
+    const endpoint = this.identityUrl + 'register';
+    return this._http
+      .post(endpoint, registerInfo)
+      .pipe(map(response => response.data));
+  }
+
+  refreshToken(refreshInfo: RefreshTokenVm) {
+    const endpoint = this.identityUrl + 'refreshToken';
+    return this._http
+      .post(endpoint, refreshInfo)
+      .pipe(map(response => response.data));
+  }
+
+  async getlistUser() {
+    return this.userRepo.findAll();
+  }
+
+  async getProfileByEmail(email: string) {
+    return this.userRepo.findOne({ where: { email } });
+  }
+
+  async updateProfile(data: any): Promise<any> {
+    const user = await this.userRepo.findOne({ where: { email: data.email } });
+
+    if (data.newPassword && data.newPassword !== data.confirmPassword) {
+      return {
+        isSuccess: true,
+        message: 'Confirm password is not match'
+      };
     }
-
-    async getUserbyEmail(userEmail: string) {
-        return this.userRepo.findOne({
-            where: { email: userEmail },
-        });
-    }
-
-    register(registerInfo: UserRegisterVm) {
-        const endpoint = this.identityUrl + 'register';
-        return this._http.post(endpoint, registerInfo)
-            .pipe(
-                map(response => response.data),
-            );
-    }
-
-    refreshToken(refreshInfo: RefreshTokenVm) {
-        const endpoint = this.identityUrl + 'refreshToken';
-        return this._http.post(endpoint, refreshInfo)
-            .pipe(
-                map(response => response.data),
-            );
-    }
-
-    async getlistUser() {
-        return this.userRepo.findAll();
-    }
-
-    async updateProfile(data: UserUpdate): Promise<any> {
-        const user = await this.userRepo.findOne({ where: { email: data.email } });
-
-        if (data.newPassword && data.newPassword !== data.confirmPassword) {
-            return {
-                isSuccess: true,
-                message: 'Confirm password is not match',
-            };
-        }
-        const hashPassword = crypto.SHA256(data.password).toString();
-        if (user) {
-            if (hashPassword !== user.password) {
-                return {
-                    isSuccess: false,
-                };
-            }
-        } else {
-            return {
-                isSuccess: false,
-            };
-        }
-
-        await this.userRepo.update({
-            address1: data.address1,
-            address2: data.address2,
-            agreement: data.agreement,
-            birthday: data.birthday,
-            city: data.city,
-            company: data.company,
-            country: data.country,
-            firstname: data.firstname,
-            gender: data.gender,
-            lastname: data.lastname,
-            phone: data.phone,
-            postcode: data.postcode,
-            regionstate: data.regionstate,
-            password: crypto.SHA256(data.newPassword).toString(),
-        },
-            {
-                where: {
-                    email: data.email,
-                },
-            });
+    const hashPassword = crypto.SHA256(data.password).toString();
+    if (user) {
+      if (hashPassword !== user.password) {
         return {
-            isSuccess: true,
-            message: 'User profile is updated successfully',
+          isSuccess: false
         };
+      }
+    } else {
+      return {
+        isSuccess: false
+      };
     }
+
+    const result = this._mapper.map(data.toJSON(), UserUpdate);
+    await this.userRepo.update(
+      {
+        address1: data.address1,
+        address2: data.address2,
+        agreement: data.agreement,
+        birthday: data.birthday,
+        city: data.city,
+        company: data.company,
+        country: data.country,
+        firstname: data.firstname,
+        gender: data.gender,
+        lastname: data.lastname,
+        phone: data.phone,
+        postcode: data.postcode,
+        regionstate: data.regionstate,
+        password: crypto.SHA256(data.newPassword).toString()
+      },
+      {
+        where: {
+          email: data.email
+        }
+      },
+    );
+    return {
+      isSuccess: true,
+      message: 'User profile is updated successfully'
+    };
+  }
 }
