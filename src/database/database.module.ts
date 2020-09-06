1a480d6a73662287d0c4712395a3c779ddc61f67
Sang Dang <sang.a5vy2@gmail.com>
2020-09-06 11:52:51 +0700

    Switch to Mongo Atlas

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { databaseProviders } from './database.providers';
 const mongoConfig =
-  'mongodb://heroku_nz29xcb8:agud4d14kekc2955rgqroci69h@ds213896.mlab.com:13896/heroku_nz29xcb8';
+  'mongodb+srv://sangdangq:nashtechfe@cluster0.dasdv.mongodb.net/nest?retryWrites=true&w=majority';
 
 @Module({
   imports: [MongooseModule.forRoot(mongoConfig)],
   providers: [...databaseProviders],
-  exports: [...databaseProviders]
+  exports: [...databaseProviders],
 })
 export class DatabaseModule {}
